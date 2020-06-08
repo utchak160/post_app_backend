3ca6d2848b90211a420df9977fa402c71f0ef311
utchak160 <chakravartyutkarsh@gmail.com>
2020-06-08 21:51:58 +0530

    ready to deploy

---
+++ middleware/check-auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const verified = jwt.verify(token, "hey_i_am_fine");
+    if (!verified) {
+      return res.status(401).send({
+        message: 'User not Authenticated'
+      });
+    }
+    req.authData = {email: verified.email, userId: verified.userId };
+    next();
+  } catch (e) {
+    res.status(401).send({
+      message: 'User not Authenticated'
+    });
+  }
+};
